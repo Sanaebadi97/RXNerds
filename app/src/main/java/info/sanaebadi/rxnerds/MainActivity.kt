f9f827e1b54ad883cd0f95cfe10007bc8aeac163
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-06 13:04:31 -0400

    create Publish Subject Variable

--- app/src/main/java/info/sanaebadi/rxnerds/MainActivity.kt
+++ app/src/main/java/info/sanaebadi/rxnerds/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.observables.ConnectableObservable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.ReplaySubject
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
@@ -26,14 +29,14 @@ class MainActivity : AppCompatActivity() {
 
         val cold: Observable<Long> = Observable.intervalRange(0, 5, 0, 1, TimeUnit.SECONDS)
         cold.subscribe {
-            Log.i(TAG, "Student 1 is : " + it)
+            //  Log.i(TAG, "Student 1 is : " + it)
 
         }
 
         Thread.sleep(3000)
 
         cold.subscribe {
-            Log.i(TAG, "Student 2 is : " + it)
+            //   Log.i(TAG, "Student 2 is : " + it)
 
         }
 
@@ -44,16 +47,134 @@ class MainActivity : AppCompatActivity() {
 
 
         hot.subscribe {
-            Log.i(TAG, "Student 1 is : " + it)
+            // Log.i(TAG, "Student 1 is : " + it)
 
         }
 
         Thread.sleep(3000)
 
         hot.subscribe {
-            Log.i(TAG, "Student 2 is : " + it)
+            // Log.i(TAG, "Student 2 is : " + it)
 
         }
+
+
+        //Hot Observable Publish Subject
+
+        val subject: PublishSubject<String> = PublishSubject.create()
+        subject.subscribe {
+                Log.i(TAG, "Student First : " + it)
+
+        }
+
+
+        subject.onNext("A")
+        sleep(1000)
+
+        subject.onNext("B")
+        sleep(1000)
+
+        subject.onNext("C")
+        sleep(1000)
+
+        subject.onNext("D")
+        sleep(1000)
+
+        subject.subscribe {
+            //  Log.i(TAG, "Student Second : " + it)
+
+        }
+
+
+
+        subject.onNext("E")
+        sleep(1000)
+
+        subject.onNext("F")
+        sleep(1000)
+
+        subject.onNext("G")
+        sleep(1000)
+
+
+        //Hot observable BehaviorSubject
+
+        val behaviorSubject: BehaviorSubject<String> = BehaviorSubject.create()
+        behaviorSubject.subscribe {
+            //   Log.i(TAG, "Student First : " + it)
+
+        }
+
+        behaviorSubject.onNext("A")
+        sleep(1000)
+
+        behaviorSubject.onNext("B")
+        sleep(1000)
+
+        behaviorSubject.onNext("C")
+        sleep(1000)
+
+        behaviorSubject.onNext("D")
+        sleep(1000)
+
+        behaviorSubject.subscribe {
+            //   Log.i(TAG, "Student Second : " + it)
+
+        }
+
+
+
+        behaviorSubject.onNext("E")
+        sleep(1000)
+
+        behaviorSubject.onNext("F")
+        sleep(1000)
+
+        behaviorSubject.onNext("G")
+        sleep(1000)
+
+
+        //Hot observable ReplaySubject
+
+        val replaySubject: ReplaySubject<String> = ReplaySubject.create()
+        replaySubject.subscribe {
+            Log.i(TAG, "Student First : " + it)
+
+        }
+
+
+        replaySubject.onNext("A")
+        sleep(1000)
+
+        replaySubject.onNext("B")
+        sleep(1000)
+
+        replaySubject.onNext("C")
+        sleep(1000)
+
+        replaySubject.onNext("D")
+        sleep(1000)
+
+        replaySubject.subscribe {
+            Log.i(TAG, "Student Second : " + it)
+
+        }
+
+
+
+        replaySubject.onNext("E")
+        sleep(1000)
+
+        replaySubject.onNext("F")
+        sleep(1000)
+
+        replaySubject.onNext("G")
+        sleep(1000)
+
     }
 
+
+    fun sleep(i: Long) {
+        Thread.sleep(i)
+    }
 }
