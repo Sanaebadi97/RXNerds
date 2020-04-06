c76d6b86b4b96c6ba86d993dd50f55e229973426
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-06 11:21:53 -0400

    define observable as cold

--- app/src/main/java/info/sanaebadi/rxnerds/MainActivity.kt
+++ app/src/main/java/info/sanaebadi/rxnerds/MainActivity.kt
@@ -2,11 +2,21 @@ package info.sanaebadi.rxnerds
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Student is Observer and Professor is Observable
+
+        //Observable is for List of data
+
+
+        val cold: Observable<Long> = Observable.intervalRange(0, 5, 0, 1, TimeUnit.SECONDS)
     }
 }
